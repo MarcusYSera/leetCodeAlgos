5be87e5fa1d1326e2625cec9aeddb6ddcacfdc7f
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-11 19:26:10 -0700

    finished setup env

---
+++ answers/kidsWithGreatestNumberofCandies.js
@@ -0,0 +1,21 @@
+var kidsWithCandies = function (candies, extraCandies) {
+  // newArr=[]
+  // largest=0
+  // for (var i = 0; i < candies.length; i++) {
+  //   if(candies[i] >= largest){
+  //     largest = candies[i];
+  //   }
+  //   // if(candies[i]+extraCandies >= )
+  // }
+  // for (var x = 0; x < candies.length; x++){
+  //   if((candies[x]+extraCandies) >= largest){
+  //     newArr.push(true);
+  //   } else {
+  //     newArr.push(false);
+  //   }
+  // }
+  const max = Math.max(...candies);
+  console.log(candies.map(x => x + extraCandies >=max))
+};
+
+kidsWithCandies([2,3,5,1,3], 3);
