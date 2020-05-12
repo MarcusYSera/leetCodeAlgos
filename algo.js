f38fcab42fb3ff9201419ed512546977ee329376
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-11 18:46:27 -0700

    first

---
+++ algo.js
@@ -0,0 +1,5 @@
+var rangeSumBST = function (root, L, R) {
+  console.log(root)
+};
+
+rangeSumBST([10, 5, 15, 3, 7, null, 18], 7, 15);
---
+++ splitAStringInBalancedStrings.js
@@ -0,0 +1,22 @@
+var balancedStringSplit = function (s) {
+  count = 0;
+  l = 0;
+  r = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == "R") {
+      r++;
+    } else if (s[i] == "L") {
+      l++;
+    }
+    if (r == l) {
+      count++;
+      r = 0;
+      l = 0;
+    }
+  }
+  console.log(count);
+  // console.log(s.length);
+  return count;
+};
+
+balancedStringSplit("RLRRLLRLRL");
