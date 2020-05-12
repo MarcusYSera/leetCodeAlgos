6f6e703e68bb5332a343800c8a33896eebfaa10f
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-11 23:45:39 -0700

    binary tree traversal

--- answers/algo.js
+++ answers/algo.js
@@ -1,5 +1,11 @@
-const rangeSumBST = function (root, L, R) {
-  console.log(root);
+var rangeSumBST = (root, L, R) => {
+  var count = 0;
+  // while (root.val !== null) {
+  //   root = root.left;
+  //   console.log(root.val);
+  // }
+  // console.log(count);
+  return count;
 };
 
 rangeSumBST([10, 5, 15, 3, 7, null, 18], 7, 15);
