acaf1cf666498432d91af40b26ad825fc6a8666e
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-12 21:26:33 -0700

    new algo

--- answers/algo.js
+++ answers/algo.js
@@ -1,11 +1,5 @@
-var rangeSumBST = (root, L, R) => {
-  var count = 0;
-  // while (root.val !== null) {
-  //   root = root.left;
-  //   console.log(root.val);
-  // }
-  // console.log(count);
-  return count;
+var destCity = function(paths) {
+  var x = 0;
 };
 
 rangeSumBST([10, 5, 15, 3, 7, null, 18], 7, 15);
