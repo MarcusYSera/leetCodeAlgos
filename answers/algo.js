c7aed65a1b9adf9fabec04e16582755937eff0df
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-14 02:32:49 -0700

    new algo

--- answers/algo.js
+++ answers/algo.js
@@ -1,23 +1,3 @@
-var destCity = function (paths) {
-  // var start = paths[0][0];
-  var fin = paths[0][1];
-  var i = 0;
-  while (paths[i + 1] != null) {
-    console.log('nom');
-    i++;
-  }
-  console.log(fin);
-  return fin;
+var oddCells = function(n, m, indices) {
+    
 };
-
-destCity([
-  ['London', 'New York'],
-  ['New York', 'Lima'],
-  ['Lima', 'Sao Paulo'],
-]);
-
-// destCity([
-//   ['B', 'C'],
-//   ['D', 'B'],
-//   ['C', 'A'],
-// ]);
