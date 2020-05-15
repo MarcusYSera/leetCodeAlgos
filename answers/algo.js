4ff1fa209460d953204b42ebb2255f9a1d2cf195
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-15 04:40:15 -0700

    part way

--- answers/algo.js
+++ answers/algo.js
@@ -5,3 +5,5 @@ var freqAlphabets = function (s) {
 
 freqAlphabets('10#11#12');
 freqAlphabets('1326#');
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
