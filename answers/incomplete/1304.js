9dcbaef533bb047966ef8bfb5165a1d06a08d2ec
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-19 23:03:51 -0700

    started 1304

---
+++ answers/incomplete/1304.js
@@ -0,0 +1,14 @@
+// Find N Unique Integers Sum up to Zero
+
+var sumZero = n => {
+  // console.log(n);
+  var newArr = [];
+  for (var i = 0; i < n; i++) {
+    console.log(i);
+  }
+};
+
+sumZero(5);
+sumZero(4);
+// sumZero(1);
+// sumZero(7);
