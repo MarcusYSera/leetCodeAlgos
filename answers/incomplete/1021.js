7abafd59e3a0ddd7e00b67e40cbfbd221698f182
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-21 23:16:57 -0700

    added 1021

---
+++ answers/incomplete/1021.js
@@ -0,0 +1,8 @@
+// 1021. Remove Outermost Parentheses
+
+var removeOuterParentheses = S => {
+  console.log('hello');
+};
+
+removeOuterParentheses('(()())(())');
+// removeOuterParentheses('(()())(())(()(()))');
