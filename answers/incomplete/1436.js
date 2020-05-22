8a4ae002b6a3122d8b022abb5468acb3f58f9328
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-21 22:49:40 -0700

    finished 1436

--- answers/incomplete/1436.js
+++ answers/incomplete/1436.js
@@ -1,21 +1,37 @@
 // 1436. Destination City
+// refactor: run time slower than 67% of other submissions
 
 var destCity = function (paths) {
   // var start = paths[0][0];
   var fin = paths[0][1];
-  var i = 0;
-  while (paths[i + 1] != null) {
-    console.log('nom');
-    i++;
+  console.log(fin);
+  for (var i = 0; i < paths.length; i++) {
+    if (fin == paths[i][0]) {
+      fin = paths[i][1];
+      i = 0;
+    }
   }
   console.log(fin);
   return fin;
 };
 
+// destCity([
+//   ['London', 'New York'],
+//   ['New York', 'Lima'],
+//   ['Lima', 'Sao Paulo'],
+// ]);
+
+// destCity([
+//   ['pYyNGfBYbm', 'wxAscRuzOl'],
+//   ['kzwEQHfwce', 'pYyNGfBYbm'],
+// ]);
+
 destCity([
-  ['London', 'New York'],
-  ['New York', 'Lima'],
-  ['Lima', 'Sao Paulo'],
+  ['qMTSlfgZlC', 'ePvzZaqLXj'],
+  ['xKhZXfuBeC', 'TtnllZpKKg'],
+  ['ePvzZaqLXj', 'sxrvXFcqgG'],
+  ['sxrvXFcqgG', 'xKhZXfuBeC'],
+  ['TtnllZpKKg', 'OAxMijOZgW'],
 ]);
 
 // destCity([
