cb5c9f9d878461468138e87b042048dee2f950af
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-22 23:06:10 -0700

    need to check for 0 within the digits

--- answers/incomplete/728.js
+++ answers/incomplete/728.js
@@ -4,9 +4,20 @@ var selfDividingNumbers = (left, right) => {
   var newArr = [];
   for (var i = left; i <= right; i++) {
     if (i > 9) {
-      console.log(i);
-    }
-    if (i % i == 0) {
+      const newNum = i.toString().split('');
+      var count = newNum.length - 1;
+      for (var x = 0; x < newNum.length - 1; x++) {
+        if (Number(newNum[x]) == 0) {
+          break;
+        }
+        if (i % Number(newNum[x]) == 0) {
+          count--;
+        }
+        if (i % Number(newNum[x]) == 0 && count === 0) {
+          newArr.push(i);
+        }
+      }
+    } else if (i % i == 0 && i < 10) {
       newArr.push(i);
     }
   }
