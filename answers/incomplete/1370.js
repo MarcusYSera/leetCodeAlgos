be02b8fb8b756b1f5de0d7b3f8dab84695085bd4
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-23 01:02:56 -0700

    adding 1370

---
+++ answers/incomplete/1370.js
@@ -0,0 +1,15 @@
+// 1370. Increasing Decreasing String
+
+var sortString = s => {
+  console.log(s);
+};
+
+sortString('aaaabbbbcccc');
+
+// sortString('rat');
+
+// sortString('leetcode');
+
+// sortString('ggggggg');
+
+// sortString('spo');
