53649b9cf39ba06a987c6358e259913dab508ab1
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-23 23:17:23 -0700

    adding 1370

--- answers/incomplete/1370.js
+++ answers/incomplete/1370.js
@@ -1,12 +1,18 @@
 // 1370. Increasing Decreasing String
 
 var sortString = s => {
-  console.log(s);
+  var newArr = [];
+  var m = s.split('');
+  for (var i = 0; i < m.length; i++) {
+    console.log(m[i]);
+    newArr.push(m.sort()[0]);
+  }
+  console.log(newArr);
 };
 
-sortString('aaaabbbbcccc');
+// sortString('aaaabbbbcccc');
 
-// sortString('rat');
+sortString('rat');
 
 // sortString('leetcode');
 
---
+++ sort/bubbleSort.js
@@ -0,0 +1,11 @@
+var bubbleSort = x => {
+  console.log(x);
+  do {
+    if (x[i] < x[y]) {
+      var swap = true;
+      const n = 
+    }
+  } while (swap != false);
+};
+
+bubbleSort([5, 10, 6, 12, 15, 1]);
