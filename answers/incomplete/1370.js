d045bedc87c263042ca325cca5ad167dc5aca414
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-10 22:53:07 -0700

    trying out differnt object creators

--- answers/incomplete/1370.js
+++ answers/incomplete/1370.js
@@ -1,8 +1,14 @@
 var sortString = function (s) {
-  var arr = [];
-  for (var i = 0; i < s.length; i++) {
-    console.log(i);
+  var alp = 'abcdefghijklmnopqrstuvwxyz';
+  var newOb = new Object();
+  for (var i = 0; i < alp.length; i++) {
+    newOb.i = alp.slice(i, i + 1);
   }
+  console.log(newOb);
+  var arr = [];
+  // for (var i = 0; i < s.length; i++) {
+  //   console.log(i);
+  // }
 };
 
 // sortString('aaaabbbbcccc');
