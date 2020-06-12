b2d61a98c291d67e8e07416155974931640b8e22
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-11 22:29:18 -0700

    recursive attempt

--- answers/incomplete/1370.js
+++ answers/incomplete/1370.js
@@ -1,36 +1,80 @@
-var sortString = function (s) {
-  const newArr = [];
-  const temp = s.split('').sort();
-  for (var i = 0; i < temp.length; ) {
-    console.log(i);
-    if (i === 0) {
-      newArr.push(temp[0]);
-      i++;
-      continue;
-    }
-    if (i === 1) {
-      if (temp[i] === newArr[newArr.length - 1]) {
-        console.log('test');
-        i++;
-        continue;
-      } else if (temp[i] !== newArr[newArr.length - 1]) {
-        console.log('test2');
-        newArr.push(temp[i]);
-        i++;
-        continue;
+var sortString = s => {
+  var turn = 'forward';
+  var newArr = [];
+  var nomers =  s =>{
+    const temp = s.split('').sort();
+    if (turn === 'forward' && temp.length !== 0) {
+      var step = 'stepOne';
+      for (var i = 0; i < temp.length; ) {
+        // console.log('run throughs should appear three times: ' + step);
+        // console.log(i);
+        if (step === 'stepOne') {
+          newArr.push(temp[0]);
+          temp.shift();
+          step = 'stepTwo';
+          continue;
+        }
+        if (step === 'stepTwo') {
+          if (temp[i] === newArr[newArr.length - 1]) {
+            // console.log('here is temp: ' + temp[i]);
+            i++;
+            step = 'stepThree';
+            continue;
+          } else if (temp[i] !== newArr[newArr.length - 1]) {
+            // console.log('test2');
+            newArr.push(temp[i]);
+            temp.splice(i, 1);
+            step = 'stepThree';
+            continue;
+          }
+        }
+        if (step === 'stepThree') {
+          step = 'stepTwo';
+          turn = 'reverse';
+          continue;
+        }
       }
     }
-    if (i === 2) {
-      i = 1;
-      continue;
+    if (temp.length !== 0 && turn === 'reverse') {
+      var step = 'stepFour';
+      for (var m = 0; m < temp.length; ) {
+        if (step === 'stepFour') {
+          newArr.push(temp[temp.length - 1]);
+          temp.pop();
+          step = 'stepFive';
+          continue;
+        }
+        if (step === 'stepFive') {
+          if (temp[temp.length - m - 1] === newArr[newArr.length - 1]) {
+            m++;
+            step = 'stepSix';
+            continue;
+          } else if (temp[temp.length - m - 1] !== newArr[newArr.length - 1]) {
+            newArr.push(temp[temp.length - m - 1]);
+            // console.log(temp[temp.length - m]);
+            temp.splice(temp.length - m - 1, 1);
+            step = 'stepSix';
+            // console.log(newArr);
+            continue;
+          }
+        }
+        if (step === 'stepSix') {
+          step = 'stepFive';
+          turn = 'forward';
+          continue;
+        }
+      }
     }
-  }
-  console.log('sorted original ' + temp);
-  console.log('newArr: ' + newArr);
-  return newArr;
+    if (temp.length !== 0) {
+      nomers(temp.join(''));
+    }
+    return newArr;
+  };
+  nomers(s);
+  return newArr.join('')
 };
 
-// sortString('aaaabbbbcccc');
-sortString('rat');
+console.log(nomers('aaaabbbbcccc'));
+// sortString('rat');
 // sortString('leetcode');
 // sortString('spo');
