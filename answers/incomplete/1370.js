2c615aaf599dd739e1bc5f7e973f958a425204cb
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-11 19:34:22 -0700

    1370 new method

--- answers/incomplete/1370.js
+++ answers/incomplete/1370.js
@@ -1,17 +1,33 @@
 var sortString = function (s) {
-  var alp = 'abcdefghijklmnopqrstuvwxyz';
-  var newOb = new Object();
-  var count = 1;
-  for (var i = 0; i < alp.length; i++) {
-    newOb[alp.slice(i, i + 1)] = i;
+  const newArr = [];
+  const temp = s.split('').sort();
+  for (var i = 0; i < temp.length; ) {
+    console.log(i);
+    if (i === 0) {
+      newArr.push(temp[0]);
+      i++;
+      continue;
+    }
+    if (i === 1) {
+      if (temp[i] === newArr[newArr.length - 1]) {
+        console.log('test');
+        i++;
+        continue;
+      } else if (temp[i] !== newArr[newArr.length - 1]) {
+        console.log('test2');
+        newArr.push(temp[i]);
+        i++;
+        continue;
+      }
+    }
+    if (i === 2) {
+      i = 1;
+      continue;
+    }
   }
-  var arr = [];
-  if(count = 1){
-    
-  }
-  for (var x = 0; x < s.length; x++) {
-    
-  }
+  console.log('sorted original ' + temp);
+  console.log('newArr: ' + newArr);
+  return newArr;
 };
 
 // sortString('aaaabbbbcccc');
