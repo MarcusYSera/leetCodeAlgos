38bde2d7c5f5c2b2999191dc076aceb56e0fe0d6
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-15 21:10:50 -0700

    finished 1464, need refactoring

---
+++ answers/incomplete/1464.js
@@ -0,0 +1,20 @@
+// 1464. Maximum Product of Two Elements in an Array
+// Refactor slower: 31% more memory: 42%
+
+var maxProduct = nums => {
+  let maxOne = 0;
+  let maxTwo = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > maxOne) {
+      maxTwo = maxOne;
+      maxOne = nums[i] - 1;
+    } else if (nums[i] > maxTwo) {
+      maxTwo = nums[i] - 1;
+    }
+  }
+  let product = maxOne * maxTwo;
+  return product;
+};
+
+// console.log(maxProduct([3, 4, 5, 2]));
+console.log(maxProduct([10, 2, 5, 2]));
