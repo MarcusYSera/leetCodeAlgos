731fc49b83c650f6c5968f4ef5eb69f02d48b65e
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-15 20:53:14 -0700

    added fin folder

--- answers/fin/kidsWithGreatestNumberofCandies.js
+++ answers/fin/kidsWithGreatestNumberofCandies.js
@@ -15,7 +15,7 @@ var kidsWithCandies = function (candies, extraCandies) {
   //   }
   // }
   const max = Math.max(...candies);
-  console.log(candies.map(x => x + extraCandies >=max))
+  console.log(candies.map(x => x + extraCandies >= max));
 };
 
-kidsWithCandies([2,3,5,1,3], 3);
+kidsWithCandies([2, 3, 5, 1, 3], 3);
