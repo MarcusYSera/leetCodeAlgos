efde2ca986f15a221fe05674b388d557475ce434
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-15 20:10:05 -0700

    finished 1480

--- answers/incomplete/1480.js
+++ answers/incomplete/1480.js
@@ -1,7 +1,14 @@
 // 1480. Running Sum of 1d Array
+// slower than 17% of other submissions and uses the least amount of memory
 
 var runningSum = nums => {
-  return nums;
+  let newArr = [];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum = nums[i] + sum;
+    newArr.push(sum);
+  }
+  return newArr;
 };
 
 console.log(runningSum([1, 2, 3, 4]));
