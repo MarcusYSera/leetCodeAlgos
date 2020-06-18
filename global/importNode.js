1efefc5f3b0fba5e97028108db25399fed3228f7
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-17 22:50:45 -0700

    constructing bst

--- global/importNode.js
+++ global/importNode.js
@@ -5,3 +5,32 @@ class Node {
     this.right = null;
   }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(data) {
+    var newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left,; newNode);
+      }
+    }else {
+      if(node.right === null){
+        node.right = newNode;
+      }else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
