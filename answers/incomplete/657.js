7a630134ef2b2c2861a04358c8228a9bfdf5612d
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-18 23:38:13 -0700

    finished 657

--- answers/incomplete/657.js
+++ answers/incomplete/657.js
@@ -1,8 +1,17 @@
 // 657. Robot Return to Origin
 
+
+
 var judgeCircle = moves => {
   moves = moves.split('');
-  return moves;
+  var direction = { U: 1, D: -1, L: 2, R: -2 };
+  let score = 0;
+  for (let i = 0; i < moves.length; i++) {
+    score = score + direction[moves[i]];
+  }
+  return score === 0 ? true : false;
 };
 
-console.log(judgeCircle('UD'));
+// console.log(judgeCircle('UD'));
+// console.log(judgeCircle('LL'));
+console.log(judgeCircle('LDRRLRUULR'));
