d7442652c3e8176fdf284562b29b65f2242a06e7
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-18 18:14:27 -0700

    finished 1475

---
+++ answers/accepted/1475.js
@@ -0,0 +1,16 @@
+// 1475. Final Prices With a Special Discount in a Shop
+// slower than 32% memory optimized
+
+var finalPrices = prices => {
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[i] >= prices[j]) {
+        prices[i] = prices[i] - prices[j];
+        break;
+      }
+    }
+  }
+  return prices;
+};
+
+console.log(finalPrices([8, 4, 6, 2, 3]));
