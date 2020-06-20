78d8c51f7ab987a61da5dd89551f4abeedd1d9d9
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-19 21:03:44 -0700

    adding 665

---
+++ answers/incomplete/665.js
@@ -0,0 +1,7 @@
+// 665. Non-decreasing Array
+
+var checkPossibility = nums => {
+  return nums;
+};
+
+console.log(checkPossibility([4, 2, 3]));
---
+++ answers/incomplete/908.js
@@ -0,0 +1,7 @@
+// 908. Smallest Range I
+
+var smallestRangeI = (A, K) => {
+  return A;
+};
+
+console.log(smallestRangeI([1], 0));
