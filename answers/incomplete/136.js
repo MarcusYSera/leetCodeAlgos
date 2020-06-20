7f64ded0d973e64b47386e8ebf8f1c15867ab3e5
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-19 20:29:41 -0700

    solved 136

--- answers/incomplete/136.js
+++ answers/incomplete/136.js
@@ -1,7 +1,13 @@
 // 136. Single Number
+// slower than 60% uses more memory than 15%
 
 var singleNumber = nums => {
-  return nums;
+  nums = nums.sort();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1] && nums[i] !== nums[i - 1]) {
+      return nums[i];
+    }
+  }
 };
 
 console.log(singleNumber([2, 2, 1]));
