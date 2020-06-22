4098b5aadf88cb144879dd086dfc467011151027
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-21 23:33:49 -0700

    working on 665

--- answers/incomplete/665.js
+++ answers/incomplete/665.js
@@ -1,14 +1,15 @@
 // 665. Non-decreasing Array
 
 var checkPossibility = nums => {
+  var count = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums.length < 6) {
       return 6 - nums.length;
     } else if (nums.length > 20) {
       return nums.length - 20;
-    } else if (){
-
-    }else {
+    } else if ((nums[i] === nums[i + 1]) === nums[i + 2]) {
+      count++;
+    } else {
       return 0;
     }
   }
