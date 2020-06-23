b9bc9303d4b9d83128c28f6cf2f0675bb7b9591a
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-22 23:30:43 -0700

    regex intialization

--- answers/incomplete/665.js
+++ answers/incomplete/665.js
@@ -1,21 +1,28 @@
 // 665. Non-decreasing Array
 
 var checkPossibility = nums => {
-  var count = 0;
-  for (let i = 0; i < nums.length; i++) {
+  if (/^(a-zA-Z0-9){6,20}$/.test(nums) === false) {
     if (nums.length < 6) {
       return 6 - nums.length;
-    } else if (nums.length > 20) {
-      return nums.length - 20;
-    } else if (/d/) {
-    } else if ((nums[i] === nums[i + 1]) === nums[i + 2]) {
-      count++;
-    } else if (count !== 0) {
-      return count;
-    } else {
-      return 0;
     }
+  } else {
+    return 0;
   }
+  // var count = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums.length < 6) {
+  //     return 6 - nums.length;
+  //   } else if (nums.length > 20) {
+  //     return nums.length - 20;
+  //   } else if (/d/) {
+  //   } else if ((nums[i] === nums[i + 1]) === nums[i + 2]) {
+  //     count++;
+  //   } else if (count !== 0) {
+  //     return count;
+  //   } else {
+  //     return 0;
+  //   }
+  // }
 };
 
 console.log(checkPossibility([4, 2, 3]));
--- answers/incomplete/regex.js
+++ answers/incomplete/regex.js
@@ -1,5 +1,6 @@
 var regEx = x => {
-  return x.match(/\d/);
+  return /^([a-zA-Z0-9]{6,20})$/.test(x);
 };
 
 console.log(regEx('hesaerASDF234'));
+console.log(regEx('hesasadfasdf23423423'));
