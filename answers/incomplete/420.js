04aa64414e6c5da3547ceee50bee8ea6f62cd432
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-22 23:44:10 -0700

    converted to string

--- answers/incomplete/420.js
+++ answers/incomplete/420.js
@@ -1,25 +1,25 @@
 // 420. Strong Password Checker
 
-var checkPossibility = nums => {
-  if (/^(a-zA-Z0-9){6,20}$/.test(nums) === false) {
-    if (nums.length < 6) {
-      console.log('nums.length < 6');
-      return 6 - nums.length;
-    } else if (nums.length > 20) {
-      console.log('nums.length > 20');
-      return nums.length - 20;
+var strongPasswordChecker = s => {
+  if (/^(a-zA-Z0-9){6,20}$/.test(s) === false) {
+    if (s.length < 6) {
+      console.log('s.length < 6');
+      return 6 - s.length;
+    } else if (s.length > 20) {
+      console.log('s.length > 20');
+      return s.length - 20;
     }
   } else {
     return 0;
   }
   // var count = 0;
-  // for (let i = 0; i < nums.length; i++) {
-  //   if (nums.length < 6) {
-  //     return 6 - nums.length;
-  //   } else if (nums.length > 20) {
-  //     return nums.length - 20;
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s.length < 6) {
+  //     return 6 - s.length;
+  //   } else if (s.length > 20) {
+  //     return s.length - 20;
   //   } else if (/d/) {
-  //   } else if ((nums[i] === nums[i + 1]) === nums[i + 2]) {
+  //   } else if ((s[i] === s[i + 1]) === s[i + 2]) {
   //     count++;
   //   } else if (count !== 0) {
   //     return count;
@@ -29,35 +29,4 @@ var checkPossibility = nums => {
   // }
 };
 
-// console.log(checkPossibility([4, 2, 3]));
-console.log(
-  checkPossibility([
-    4,
-    5,
-    6,
-    1,
-    0,
-    2,
-    3,
-    5,
-    3,
-    2,
-    1,
-    3,
-    4,
-    4,
-    2,
-    5,
-    3,
-    'A',
-    'b',
-    'T',
-    2,
-    9,
-    ,
-    'H',
-    2,
-    9,
-    8,
-  ]),
-);
+console.log(strongPasswordChecker('aaa123'));
--- answers/incomplete/665.js
+++ answers/incomplete/665.js
@@ -1,2 +1,1 @@
 // 665. Non-decreasing Array
-
