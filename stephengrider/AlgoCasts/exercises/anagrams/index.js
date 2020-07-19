05b9055b9b30fbfba4d6fc3f685c40a0513cf30a
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-18 23:08:38 -0700

    anagram and chunk practice

--- stephengrider/AlgoCasts/exercises/anagrams/index.js
+++ stephengrider/AlgoCasts/exercises/anagrams/index.js
@@ -11,13 +11,27 @@
 function anagrams(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
-  function buildCharMap(str) {
-    const charMap = {};
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-      charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap;
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
   }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
   return charMap;
 }
 
