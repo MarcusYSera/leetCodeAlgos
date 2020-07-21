76fb0a2c5cb8821554cbb01d2263c38cabd9855f
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-20 21:25:51 -0700

    practice

--- stephengrider/AlgoCasts/exercises/capitalize/index.js
+++ stephengrider/AlgoCasts/exercises/capitalize/index.js
@@ -7,13 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  let strSplit = str.split(' ');
-  let answer = [];
-  for (let word of strSplit) {
-    answer.push(word[0].toUpperCase() + word.slice(1));
-  }
-  return answer.join(' ');
-}
+function capitalize(str) {}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
+
+// function capitalize(str) {
+//   let strSplit = str.split(' ');
+//   let answer = [];
+//   for (let word of strSplit) {
+//     answer.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return answer.join(' ');
+// }
