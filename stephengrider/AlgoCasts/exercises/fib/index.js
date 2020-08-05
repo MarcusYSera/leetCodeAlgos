034bd2875650d53efeb7a962dc4bcda002008109
Marcus Sera <m.yoshiosera@gmail.com>
2020-08-04 21:10:17 -0700

    reset

--- stephengrider/AlgoCasts/exercises/fib/index.js
+++ stephengrider/AlgoCasts/exercises/fib/index.js
@@ -8,25 +8,7 @@
 // Example:
 //   fib(4) === 3
 
-const memoize = fn => {
-  const cache = {};
-  return (...args) => {
-    if (cache[args]) {
-      return cache[args];
-    }
-    const result = fn.apply(this, args);
-    cache[args] = result;
-
-    return result;
-  };
-};
-
-const slowFib = n => {
-  if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
-};
-
-const fib = memoize(slowFib);
+const fib = n => {};
 
 module.exports = fib;
 
