258f9b0615d2bad2b9a49d5d683926844acafc17
Marcus Sera <m.yoshiosera@gmail.com>
2020-08-05 23:44:35 -0700

    memoizationnnn

--- stephengrider/AlgoCasts/exercises/fib/index.js
+++ stephengrider/AlgoCasts/exercises/fib/index.js
@@ -8,17 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-const fib = n => {
-  const result = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
-
-    result.push(a + b);
-  }
-  return result[n];
+const memoize = fn => {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
 };
+
+const slowFib = n => {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
