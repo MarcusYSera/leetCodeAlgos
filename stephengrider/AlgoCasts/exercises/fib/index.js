8e3ee5d45d29facd2a8439611beea992303faf2f
Marcus Sera <m.yoshiosera@gmail.com>
2020-08-05 23:30:20 -0700

    mock

--- stephengrider/AlgoCasts/exercises/fib/index.js
+++ stephengrider/AlgoCasts/exercises/fib/index.js
@@ -8,12 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-const fib = n => {
+const memoize = fn => {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+};
+
+const slowFib = n => {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 };
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
