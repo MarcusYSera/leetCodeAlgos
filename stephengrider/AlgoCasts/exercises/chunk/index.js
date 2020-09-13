df3c7ac968eadea5c123b75eae4222a8a59205bd
Marcus Sera <m.yoshiosera@gmail.com>
2020-09-12 23:36:39 -0700

    chunking sections of an array

--- stephengrider/AlgoCasts/exercises/chunk/index.js
+++ stephengrider/AlgoCasts/exercises/chunk/index.js
@@ -9,7 +9,13 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
 }
 
 module.exports = chunk;
@@ -18,7 +24,6 @@ module.exports = chunk;
 //   const chunked = [];
 
 //   for (let element of array) {
-//     console.log(element);
 //     const last = chunked[chunked.length - 1];
 
 //     if (!last || last.length === size) {
--- stephengrider/AlgoCasts/exercises/maxchar/index.js
+++ stephengrider/AlgoCasts/exercises/maxchar/index.js
@@ -5,11 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  
-}
+function maxChar(str) {}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+// let charMap = {};
+// let max = 0;
+// let charMax = '';
+// for (let char of str) {
+//   if (charMap[char]) {
+//     charMap[char]++;
+//   } else {
+//     charMap[char] = 1;
+//   }
+// }
+// for(let char in charMap){
+//   if(charMap[char] > max){
+//     max = charMap[char];
+//     charMax = char;
+//   }
+// }
+// return charMax;
+// }
 
 // function maxChar(str) {
 //   let charMap = {};
